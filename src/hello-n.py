dabd3bb02d3de264cd4d778b7921d7665a931908
Noa6699 <112637593+Noa6699@users.noreply.github.com>
2022-09-09 08:57:28 +0200

    hello

--- src/hello-n.py
+++ src/hello-n.py
@@ -4,5 +4,13 @@
 # Now, print "Hello, World!" n times.
 
 n=int(input("How many times should I loop"))
-for x in range(0, n):
+for x in range(n):
     print("Hello, World!")
+
+
+
+#la version while
+#x = 0
+#while x<n:
+ #   print("Hello, World!")
+#    x=1
